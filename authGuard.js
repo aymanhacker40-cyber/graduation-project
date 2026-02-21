c06c609471d562c8caa6e289f40ead9d6605650b
Ayman <aymanhacker40@gmail.com>
2026-02-21 11:22:04 +0200

    connect production api

--- authGuard.js
+++ authGuard.js
@@ -1,9 +1,7 @@
 // check token from local OR session storage
-const token =
-  localStorage.getItem("token") ||
-  sessionStorage.getItem("token");
+const token = localStorage.getItem("token") || sessionStorage.getItem("token");
 
 if (!token) {
   window.location.href =
-    window.location.origin + "/registration/login/travel_login_html.html";
+    window.location.origin + "/frontend/registration/login/login.html";
 }
